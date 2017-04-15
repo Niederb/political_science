6ac36cab3cc0ec5d3666fbdcf9c9e470f5d79e9c
niederb <thomas@niederberger.ch>
2017-04-15 22:02:52 +0200

    Improve PCA analysis

--- PCA-ja-stimmen/pca_analyse.py
+++ PCA-ja-stimmen/pca_analyse.py
@@ -29,9 +29,9 @@ fig, ax = plt.subplots()
 index = np.arange(len(cantons))
 colors = cm.hsv(np.linspace(0, 1, len(cantons)))
 for i, col in zip(index, colors):
-    tmp = plt.scatter(coordinates[i, 0], coordinates[i, 1], color=col, label=cantons[i])
+    tmp = plt.scatter(coordinates[i, 0], coordinates[i, 1], color=col, label=cantons[i], s=50)
 #plt.legend()
-    plt.text(coordinates[i, 0], coordinates[i, 1], cantons[i])
+    plt.text(coordinates[i, 0] + 2, coordinates[i, 1] - 2, cantons[i], size=8)
     
 n_questions = 10    
 questions = pca.components_
@@ -41,7 +41,9 @@ indexsort = np.argsort(weight)[-n_questions:]
 index = np.arange(n_questions)
 colors = cm.hsv(np.linspace(0, 1, n_questions))
 plt.figure()
+ax = plt.axes()
 ids = importdata["id"][indexsort]
 for i, col in zip(indexsort, colors):
+    ax.arrow(0, 0, questions[0, i], questions[1, i], head_width=0.01, head_length=0.01, fc=col, ec=col, color=col)    
     tmp = plt.scatter(questions[0, i], questions[1, i], color=col, label=ids[i])
     plt.text(questions[0, i], questions[1, i], ids[i])
