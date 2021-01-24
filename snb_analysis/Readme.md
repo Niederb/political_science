70e906d1f2c327430db9a59bff6d6196e3ce216c
Thomas Niederberger <thomas@niederb.ch>
2021-01-24 19:06:46 +0100

    Refactoring and documentation

--- snb_analysis/Readme.md
+++ snb_analysis/Readme.md
@@ -6,7 +6,40 @@
 
 For this small analysis I decided to just use a pdf library and a plotting library. The actual data analysis part is basically non-existent, so I decided against using a data analysis library.
 
+## Processing
 
-## Pro and con
+### Getting report lengths
 
-One thing that I really like with Rust is, how many errors the compiler can find. Especially with hobby projects, on which I only work a 
+For this we need to get the page lengths from all the pdfs. Luckly there are multiple pdf crates available. I decided to go with the [lopdf](https://crates.io/crates/lopdf) crate.
+
+Luckily the plotly crate makes it quite easy to load a document and get the number of pages out of it:
+
+```rust
+let d = lopdf::Document::load(&path).unwrap();
+let number_of_pages = d.get_pages().len();
+```
+
+So we just iterate over all the files and get the information we need out of them. Afterwards we just create two vectors: One with the year, which we extract from the folder name, and one with the extracted page numbers.
+
+### Creating the plot
+
+For plotting I decided to go with the [plotly](https://crates.io/crates/plotly) crate which provides an API to [plotly](https://plotly.com/javascript/). We just pass in out two vectors (`year` and `number_of_pages`) into `Bar::new(..)`, add a title and names for the axis:
+
+```rust
+    let trace = Bar::new(years, report_lengths)
+        .name("report_lengths");
+    let layout = Layout::new()
+        .title(Title::new("Swiss National Bank Annual Report Length"))
+        .x_axis(Axis::new().title(Title::new("Year")))
+        .y_axis(Axis::new().title(Title::new("Pages")));
+    let mut plot = Plot::new();
+    plot.set_layout(layout);
+    plot.add_trace(trace);
+    plot.show();
+```
+
+## Conclusion
+
+Overall I was easier than expected to create this simple analysis. Finding and selecting the libraries that I neede took probably most of the time.
+
+One thing that I really like with Rust is, how many errors the compiler can find. Especially with hobby projects, on which I only work every couple of weeks.
--- snb_analysis/src/main.rs
+++ snb_analysis/src/main.rs
@@ -1,13 +1,12 @@
 use walkdir::WalkDir;
 
-use plotly::common::{
-    ColorScale, ColorScalePalette, DashType, Fill, Font, Line, LineShape, Marker, Mode, Title,
-};
-use plotly::layout::{Axis, BarMode, Layout, Legend, TicksDirection};
-use plotly::{Bar, NamedColor, Plot, Rgb, Rgba, Scatter};
+use plotly::common::Title;
+use plotly::layout::{Axis, Layout};
+use plotly::{Bar, Plot};
 
-fn main() {
-    let path = r"D:\Repository\annual_report_database\downloads\2021-01-08\Schweizerische Nationalbank";
+use std::env;
+
+fn get_report_lengths(path: &str) -> (Vec<String>, Vec<usize>) {
     let walker = WalkDir::new(path).into_iter();
 
     let mut years = Vec::new();
@@ -19,27 +18,40 @@ fn main() {
             if let Some(ext) = os_ext.to_str() {
                 if ext == "pdf" {
                     let d = lopdf::Document::load(&path).unwrap();
-                    let page_number = d.get_pages().len();
+                    let number_of_pages = d.get_pages().len();
                     let year_string = path.parent().unwrap().file_stem().unwrap();
                     let year_string = year_string.to_str().unwrap().to_owned();
-                    println!("{:?};{}", &year_string, &page_number);
+                    println!("{:?};{}", &year_string, &number_of_pages);
 
                     years.push(year_string);
-                    report_lengths.push(page_number);
+                    report_lengths.push(number_of_pages);
                 }
             }
         }
     }
-    let trace = Scatter::new(years, report_lengths)
-        .mode(Mode::LinesMarkers)
-        .line(Line::new().width(7.0))
-        .name("Scatter");
-    // https://www.snb.ch/de/iabout/pub/id/pub_annrep
-    let layout = Layout::new().title(Title::new("Swiss National Bank Annual Report Length"))
+    (years, report_lengths)
+}
+
+fn create_plot(years: Vec<String>, report_lengths: Vec<usize>) {
+    let trace = Bar::new(years, report_lengths).name("report_lengths");
+    let layout = Layout::new()
+        .title(Title::new("Swiss National Bank Annual Report Length"))
         .x_axis(Axis::new().title(Title::new("Year")))
         .y_axis(Axis::new().title(Title::new("Pages")));
     let mut plot = Plot::new();
     plot.set_layout(layout);
     plot.add_trace(trace);
     plot.show();
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 1 {
+        println!("Usage: snp_analysis <directory_with_reports>");
+    } else {
+        let path = &args[0];
+        let (years, report_lengths) = get_report_lengths(&path);
+        create_plot(years, report_lengths);
+    }
 }
